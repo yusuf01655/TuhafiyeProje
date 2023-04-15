5d24cc16c0c428b341770995517702b49d95322a
Yusuf Aksoy <yusufaksoy.mdbf19@iste.edu.tr>
2023-04-15 11:23:23 +0300

    Main.java'da fxml anchorpane'in kaynagi degistirildi.

--- src/tr/com/yusuf/test/Main.java
+++ src/tr/com/yusuf/test/Main.java
@@ -1,5 +1,4 @@
 package tr.com.yusuf.test;
-	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,7 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/tr/com/yusuf/fe/Sample.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
