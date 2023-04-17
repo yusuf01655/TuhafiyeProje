2e95659732832cad7c2f921af717e413d60e04c4
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-04-17 14:25:26 +0300

    HesaplarContract sinifina attributelar ve getter ve setter fonksiyonlari eklendi

--- src/tr/com/yusuf/types/HesaplarContract.java
+++ src/tr/com/yusuf/types/HesaplarContract.java
@@ -2,8 +2,42 @@ package tr.com.yusuf.types;
 
 public class HesaplarContract {
 
-	public HesaplarContract() {
-		// TODO Auto-generated constructor stub
+	private int id;
+	private String ad;
+	private int yetkiId;
+	private int personelId;
+	private String sifre;
+	
+	public int getId(){
+		return this.id;
 	}
-
+	public void setId(int id){
+		this.id = id;
+	}
+	public String getAd(){
+		return this.ad;
+	}
+	public void setAd(String ad){
+		this.ad = ad;
+	}
+	public int getYetkiId(){
+		return this.yetkiId;
+	}
+	public void setYetkiId(int yetkiId){
+		this.yetkiId = yetkiId;
+	}
+	public void getPersonelId(int personelId){
+		this.personelId = personelId;
+	}
+	public String getSifre(){
+		return this.sifre;
+	}
+	public void setSifre(String sifre){
+		this.sifre = sifre;
+	}
+	
+	@Override 
+	public String toString(){
+		return String.format("%d %s %d %d %s");
+	} 
 }
