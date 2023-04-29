2340e692bdcda3780558ecba3658ca27c743cbb9
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-04-29 22:31:20 +0300

    veritabani tablolarinda normalizasyon islemi yapildi.

--- veritabani tablolar.sql
+++ veritabani tablolar.sql
@@ -1,51 +1,76 @@
-CREATE TABLE  MUSTERI(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	AD_SOYAD VARCHAR(255),
-	TELEFON VARCHAR(255),
-	ADRES VARCHAR(255),
-	SEHIR_ID INT);
+	create table  musteri(
+	musteri_id int auto_increment primary key not null,
+	ad varchar(255),
+	soyad varchar(255),
+	telefon varchar(255),
+	adres varchar(255),
+	sehir_id int,
+	foreign key (sehir_id) references sehir(sehir_id)
+	);
 	
-	CREATE TABLE  ACCOUNTS(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	YETKI_ID INT,
-	SIFRE VARCHAR(255),
-	PERSONEL_ID INT);
+	create table  hesap(
+	hesap_id int auto_increment primary key not null,
+	yetki_id int,
+	sifre varchar(255),
+	personel_id int,
+	foreign key (yetki_id) references yetki(yetki_id),
+	foreign key (hesap_id) references hesap(hesap_id),
+	foreign key (personel_id) references personel(personel_id)
+	);
 	
-	CREATE TABLE  KATEGORI(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	ADI VARCHAR(255),
-	PARENT_ID INT);
+	create table  kategori(
+	kategori_id int auto_increment primary key not null,
+	adi varchar(255),
+	ust_kategori_id int, 
+	foreign key (ust_kategori_id) references ust_kategori(ust_kategori_id)
+	);
 	
-	CREATE TABLE  PERSONEL(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	AD_SOYAD VARCHAR(255),
-	EMAIL VARCHAR(255));
+	create table  personel(
+	personel_id int auto_increment primary key not null,
+	ad varchar(255)
+	soyad varchar(255),
+	email varchar(255),
+	foreign key(personel_id) references personel(personel_id)
+	);
 	
-	CREATE TABLE  SATIS(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	MUSTERI_ID INT,
-	PERSONEL_ID INT,
-	URUN_ID INT,
-	ADET INT,
-	TARIH DATE);
+	create table  satis(
+	satis_id int auto_increment primary key not null,
+	musteri_id int,
+	personel_id int,
+	urun_id int,
+	adet int,
+	tarih date,
+	foreign key (musteri_id) references musteri(musteri_id),
+	foreign key(personel_id) references personel(personel_id),
+	foreign key(urun_id) references urun(urun_id));
 	
-	CREATE TABLE  STOK(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	PERSONEL_ID INT,
-	URUN_ID INT,
-	TARIH DATE,
-	ADET INT);
+	create table  stok(
+	stok_id int auto_increment primary key not null,
+	personel_id int,
+	urun_id int,
+	tarih date,
+	adet int,
+	foreign key(personel_id) references personel(personel_id),
+	foreign key(urun_id) references urun(urun_id)
+	);
 	
-	CREATE TABLE  URUNLER(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	AD VARCHAR(255),
-	KATEGORI_ID INT,
-	TARIH DATE,
-	FIYAT DECIMAL);
+	create table  urun(
+	urun_id int auto_increment primary key not null,
+	ad varchar(255),
+	kategori_id int,
+	tarih date,
+	fiyat decimal,
+	foreign key(kategori_id) references kategori(kategori_id));
 	
-	CREATE TABLE  YETKILER(
-	ID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	ADI VARCHAR(255));
+	create table  yetki(
+	yetki_id int auto_increment primary key not null,
+	adi varchar(255));
 	
-	
-	
+	create table sehir(
+		sehir_id int auto_increment primary key not null,
+		ad varchar(255);
+	);
+	create table ust_kategori(
+		ust_kategori_id int,
+		adi varchar(255)
+	);
