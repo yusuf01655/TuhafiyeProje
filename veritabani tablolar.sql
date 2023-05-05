1177c0bd35c7ffd2d384e4e54d34c8167d8a5f95
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-05-05 22:00:35 +0300

    veritabani tablolari ayarlandi.

--- veritabani tablolar.sql
+++ veritabani tablolar.sql
@@ -1,32 +1,40 @@
+	CREATE DATABASE tuhafiye_otomasyonu;
 	CREATE TABLE musteriler(
-	    musteri_id INT          DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	    PRIMARY KEY(musteri_id),
+	    musteri_id INT          DEFAULT 1 AUTO_INCREMENT  NOT NULL,
 	    ad         VARCHAR(255) DEFAULT 'ad',
 	    soyad      VARCHAR(255) DEFAULT 'soyad',
 	    telefon    VARCHAR(255) DEFAULT 'telefon',
 	    adres      VARCHAR(255) DEFAULT 'adres',
 	    sehir_id   INT          DEFAULT 1,
+	    
 	    FOREIGN_KEY(sehir_id) REFERENCES sehir(sehir_id)
+	    
 	);
 	
 	CREATE TABLE hesaplar(
-	    hesap_id    INT          DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+            PRIMARY KEY(hesap_id),
+	    hesap_id    INT          DEFAULT 1 AUTO_INCREMENT NOT NULL,
 	    yetki_id    INT          DEFAULT 1,
 	    sifre       VARCHAR(255) DEFAULT 'sifre' ,
 	    personel_id INT          DEFAULT 1,
+	   
 	    FOREIGN KEY(yetki_id)    REFERENCES yetki(yetki_id),
 	    FOREIGN KEY(hesap_id)    REFERENCES hesap(hesap_id),
 	    FOREIGN KEY(personel_id) REFERENCES personel(personel_id)
 	);
 	
 	CREATE TABLE kategoriler(
-	    kategori_id     INT          DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	    PRIMARY KEY (kategori_id),
+	    kategori_id     INT          DEFAULT 1 AUTO_INCREMENT NOT NULL,
 	    ad              VARCHAR(255) DEFAULT 'ad',
 	    ust_kategori_id INT          DEFAULT 1,
 	    FOREIGN KEY(ust_kategori_id) REFERENCES ust_kategori(ust_kategori_id)
 	);
 	
 	CREATE TABLE personeller(
-	    personel_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	    PRIMARY KEY (personel_id),
+	    personel_id INT AUTO_INCREMENT NOT NULL,
 	    ad          VARCHAR(255) DEFAULT 'ad',
   	    soyad       VARCHAR(255) DEFAULT 'soyad',
 	    email       VARCHAR(255) DEFAULT 'ornek@ornek.com',
@@ -34,7 +42,8 @@
 	);
 	
 	CREATE TABLE  satislar(
-	    satis_id    INT  DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	    PRIMARY KEY (satis_id),
+	    satis_id    INT  DEFAULT 1 AUTO_INCREMENT NOT NULL,
 	    musteri_id  INT  DEFAULT 1,
 	    personel_id INT  DEFAULT 1,
 	    urun_id     INT  DEFAULT 1,
@@ -46,7 +55,8 @@
 	);
 	
 	CREATE TABLE  stoklar(
-	    stok_id     INT DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	    PRIMARY KEY(stok_id),
+	    stok_id     INT DEFAULT 1 AUTO_INCREMENT NOT NULL,
 	    personel_id INT DEFAULT 1,
 	    urun_id     INT DEFAULT 1,
 	    tarih       DATE DEFAULT '1990-01-01 00:00:00.00000',
@@ -56,7 +66,8 @@
 	);
 	
 	CREATE TABLE  urunler(
-	    urun_id     INT DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
+            PRIMARY KEY (urun_id),
+	    urun_id     INT DEFAULT 1 AUTO_INCREMENT NOT NULL,
 	    ad          VARCHAR(255) DEFAULT 'ad' ,
 	    kategori_id INT DEFAULT 1,
 	    tarih       DATE DEFAULT '1990-01-01 00:00:00.00000',
@@ -65,15 +76,20 @@
 	);
 	
 	CREATE TABLE  yetkiler(
-	    yetki_id INT DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	    ad       VARCHAR(255) DEFAULT 'ad'
+	    PRIMARY KEY (yetki_id),
+	    yetki_id INT DEFAULT 1 AUTO_INCREMENT NOT NULL,
+	    ad       VARCHAR(255) DEFAULT 'ad',
 	);
 	
 	CREATE TABLE sehirler(
-	    sehir_id INT DEFAULT 1 AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	    ad       VARCHAR(255) DEFAULT 'ad';
+	    PRIMARY KEY(sehir_id),
+	    sehir_id INT DEFAULT 1 AUTO_INCREMENT NOT NULL,
+	    ad       VARCHAR(255) DEFAULT 'ad'
+
 	);
 	CREATE TABLE ust_kategoriler(
-	    ust_kategori_id INT DEFAULT 1,
-	    adi             VARCHAR(255) DEFAULT 'ad'
+	    PRIMARY KEY(ust_kategori_id)
+	    ust_kategori_id INT DEFAULT 1 NOT NULL,
+	    adi             VARCHAR(255) DEFAULT 'ad',
+  	    
 	);
