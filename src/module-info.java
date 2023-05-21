387d9dd7858e193ef92e1a3c5bd2b2decda49b46
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-05-21 23:19:13 +0300

    yetki ekleme islemi yapildi.

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module TuhafiyeProje {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires javafx.graphics;
 
 	exports tr.com.yusuf.test;
 
--- src/tr/com/yusuf/fe/YetkiEkleFE.java
+++ src/tr/com/yusuf/fe/YetkiEkleFE.java
@@ -1,9 +1,46 @@
 package tr.com.yusuf.fe;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import tr.com.yusuf.dal.YetkilerDAL;
+import tr.com.yusuf.types.YetkilerContract;
+
 public class YetkiEkleFE {
 
-	public YetkiEkleFE() {
-		// TODO Auto-generated constructor stub
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private URL location;
+
+	@FXML
+	private Button btn_ekle;
+	@FXML
+	private TextField textFieldYetkiAdi;
+
+	@FXML
+	void btnEkle_OnAction(ActionEvent event) {
+		YetkilerContract yetkilerContract = new YetkilerContract();
+		yetkilerContract.setAd(textFieldYetkiAdi.getText());
+		new YetkilerDAL().ekle(yetkilerContract);
+
+		// bilgi mesaji
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Ekleme iþlemi");
+		alert.setHeaderText(null);
+		alert.setContentText("Yetki basariyla eklendi.");
+		alert.showAndWait();
 	}
 
+	@FXML
+	void initialize() {
+
+	}
 }
