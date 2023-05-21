e826187d3af64f669d8ccb7b23d07e7c51d49a17
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-05-21 23:20:21 +0300

    urlnin varsayilan degeri silindi.

--- src/tr/com/yusuf/core/CoreFields.java
+++ src/tr/com/yusuf/core/CoreFields.java
@@ -4,7 +4,7 @@ public class CoreFields {
 
 	private String kullaniciAdi;
 	private String sifre;
-	private String url = "jdbc:mysql://localhost:3306/tuhafiye_otomasyonu?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
+	private String url;
 
 	public String getKullaniciAdi() {
 		return kullaniciAdi;
