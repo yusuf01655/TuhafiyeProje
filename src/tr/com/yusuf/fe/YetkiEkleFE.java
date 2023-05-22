5f45db0198c715485d5f87527af45358110d499a
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-05-22 15:59:53 +0300

    Pencere baslatma islemi eklendi

--- src/tr/com/yusuf/fe/YetkiEkleFE.java
+++ src/tr/com/yusuf/fe/YetkiEkleFE.java
@@ -5,14 +5,19 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import tr.com.yusuf.dal.YetkilerDAL;
+import tr.com.yusuf.interfaces.FeInterfaces;
 import tr.com.yusuf.types.YetkilerContract;
 
-public class YetkiEkleFE {
+public class YetkiEkleFE implements FeInterfaces {
 
 	@FXML
 	private ResourceBundle resources;
@@ -41,6 +46,21 @@ public class YetkiEkleFE {
 
 	@FXML
 	void initialize() {
+
+	}
+
+	@Override
+	public void initPencere() {
+		try {
+			Stage primaryStage = new Stage();
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/tr/com/yusuf/fe/YetkiEkleFE.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("/tr/com/yusuf/test/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 }
