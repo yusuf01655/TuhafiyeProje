375f0c9db4770bc0ceaa44ea96fcb822b9e1d8f5
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-05-22 15:58:23 +0300

    pencere acma islemi icin kod kalabaligi kaldirildi

--- src/tr/com/yusuf/fe/AnaPencereFEController.java
+++ src/tr/com/yusuf/fe/AnaPencereFEController.java
@@ -5,11 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class AnaPencereFEController {
 	@FXML
@@ -23,20 +19,13 @@ public class AnaPencereFEController {
 
 	@FXML
 	void menuItemYetkiEkle_OnAction(ActionEvent event) {
-		try {
-			Stage primaryStage = new Stage();
-			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/tr/com/yusuf/fe/YetkiEkleFE.fxml"));
-			Scene scene = new Scene(root, 400, 400);
-			scene.getStylesheets().add(getClass().getResource("/tr/com/yusuf/test/application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		new YetkiEkleFE().initPencere();
+
 	}
 
 	@FXML
 	void initialize() {
 
 	}
+
 }
