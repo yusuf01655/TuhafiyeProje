a76050bfd8cc74f8a37a0be9abf7d5d83a54dd3c
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-05-26 06:44:19 +0300

    HesaplarDAL sinifinin fonksiyonlari dolduruldu

--- src/tr/com/yusuf/dal/HesaplarDAL.java
+++ src/tr/com/yusuf/dal/HesaplarDAL.java
@@ -1,44 +1,150 @@
 package tr.com.yusuf.dal;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import tr.com.yusuf.core.ObjectHelper;
 import tr.com.yusuf.interfaces.DALInterfaces;
+import tr.com.yusuf.types.HesaplarContract;
 
-public class HesaplarDAL<HesaplarContract> extends ObjectHelper implements DALInterfaces<HesaplarContract> {
-
-	public HesaplarDAL() {
-		// TODO Auto-generated constructor stub
-	}
+public class HesaplarDAL extends ObjectHelper implements DALInterfaces<HesaplarContract> {
 
 	@Override
 	public void ekle(HesaplarContract entity) {
-		// TODO Auto-generated method stub
+		Connection connection = getConnection();
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("INSERT INTO hesaplar(yetki_id, sifre, personel_id) VALUES(" + entity.getYetkiId()
+					+ ", " + entity.getSifre() + ", " + entity.getPersonelId() + ")");
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public List<HesaplarContract> hepsiniAl() {
-		// TODO Auto-generated method stub
-		return null;
+		List<HesaplarContract> veriContract = new ArrayList<>();
+		Connection connection = getConnection();
+		HesaplarContract hesaplarContract;
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM hesaplar");
+			while (resultSet.next()) {
+				hesaplarContract = new HesaplarContract();
+				hesaplarContract.setId(resultSet.getInt("hesap_id"));
+				hesaplarContract.setYetkiId(resultSet.getInt("yetki_id"));
+				hesaplarContract.setSifre(resultSet.getString("sifre"));
+				hesaplarContract.setPersonelId(resultSet.getInt("personel_id"));
+				veriContract.add(hesaplarContract);
+			}
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		return veriContract;
 	}
 
 	@Override
 	public HesaplarContract sil(HesaplarContract entity) {
-		// TODO Auto-generated method stub
-		return null;
+		HesaplarContract hesaplarContract = new HesaplarContract();
+		Connection connection = getConnection();
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("DELETE FROM hesaplar WHERE hesap_id=" + entity.getId());
+			statement.close();
+			connection.close();
+
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		return hesaplarContract;
 	}
 
 	@Override
 	public void guncelle(HesaplarContract entity) {
-		// TODO Auto-generated method stub
+		Connection connection = getConnection();
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("UPDATE hesaplar SET yetki_id = " + entity.getYetkiId() + ", sifre = '"
+					+ entity.getSifre() + "', personel_id = " + entity.getPersonelId() + " WHERE hesap_id = "
+					+ entity.getId() + "");
+			statement.close();
+			connection.close();
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public List<HesaplarContract> idIleAl(int id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<HesaplarContract> veriContract = new ArrayList<>();
+		Connection connection = getConnection();
+		HesaplarContract hesaplarContract;
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM hesaplar WHERE hesap_id = " + id);
+			while (resultSet.next()) {
+				hesaplarContract = new HesaplarContract();
+				hesaplarContract.setId(resultSet.getInt("hesap_id"));
+				hesaplarContract.setYetkiId(resultSet.getInt("yetki_id"));
+				hesaplarContract.setSifre(resultSet.getString("sifre"));
+				hesaplarContract.setPersonelId(resultSet.getInt("personel_id"));
+
+				veriContract.add(hesaplarContract);
+
+			}
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		return veriContract;
 	}
 
+	public HesaplarContract getPersonelIdVeSifre(int personel_id, String sifre) {
+		HesaplarContract contract = new HesaplarContract();
+
+		Connection connection = getConnection();
+
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM hesaplar WHERE personel_id = " + personel_id
+					+ " AND sifre = '" + sifre.trim() + "'");
+
+			while (resultSet.next()) {
+				contract.setId(resultSet.getInt("Id"));
+				contract.setPersonelId(resultSet.getInt("PersonelId"));
+				contract.setSifre((resultSet.getString("Sifre")));
+				contract.setYetkiId(resultSet.getInt("YetkiId"));
+			}
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+		}
+		return contract;
+	}
+
+	public HesaplarContract GetYetkiId(int personel_id) {
+		HesaplarContract contract = new HesaplarContract();
+		Connection connection = getConnection();
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement
+					.executeQuery("SELECT * FROM hesaplar WHERE personel_id = " + personel_id + " ");
+
+			while (resultSet.next()) {
+				contract.setId(resultSet.getInt("hesap_id"));
+				contract.setPersonelId(resultSet.getInt("personel_id"));
+				contract.setYetkiId(resultSet.getInt("yetki_id"));
+			}
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+		}
+		return contract;
+	}
 }
