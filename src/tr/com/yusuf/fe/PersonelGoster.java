2cd7c83c4063989ef2486a86dfb5ce169cd1805e
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-17 07:21:56 +0300

    PersonelGoster, PersonelSilFE, UrunDuzenleFE, UrunGosterFE, UrunSilFE siniflari olusturuldu

---
+++ src/tr/com/yusuf/fe/PersonelGoster.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class PersonelGoster implements FeInterfaces {
+
+	public PersonelGoster() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tr/com/yusuf/fe/PersonelSilFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class PersonelSilFE implements FeInterfaces {
+
+	public PersonelSilFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
