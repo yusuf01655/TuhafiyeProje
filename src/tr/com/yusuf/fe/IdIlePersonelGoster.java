e93f1ece4a518cb6a679ed8249892998ddf01d92
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-17 07:18:56 +0300

    IdIlePersonelGoster IdIleUrunGosterFE MusteriDuzenleFE siniflari olusturuldu

---
+++ src/tr/com/yusuf/fe/IdIlePersonelGoster.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class IdIlePersonelGoster implements FeInterfaces {
+
+	public IdIlePersonelGoster() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tr/com/yusuf/fe/IdIleUrunGosterFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class IdIleUrunGosterFE implements FeInterfaces {
+
+	public IdIleUrunGosterFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
