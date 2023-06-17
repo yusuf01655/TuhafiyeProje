62e9f53513adda5c2072d8bd3832af6e715d0b81
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-17 17:32:39 +0300

    Kategori ekle, sil, goster, duzenle, id ile goster siniflari eklendi

---
+++ src/tr/com/yusuf/fe/KategoriGosterFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class KategoriGosterFE implements FeInterfaces {
+
+	public KategoriGosterFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tr/com/yusuf/fe/KategoriSilFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class KategoriSilFE implements FeInterfaces {
+
+	public KategoriSilFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
