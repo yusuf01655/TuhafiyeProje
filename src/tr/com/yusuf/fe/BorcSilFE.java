e799c72db83032ee80d9e0c9e879278e3674c1c9
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-17 07:16:37 +0300

    BorcSilFE IdIleBorcGosterFE IdIleMusteriGosterFE siniflari olusturuldu

---
+++ src/tr/com/yusuf/fe/BorcSilFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class BorcSilFE implements FeInterfaces {
+
+	public BorcSilFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
