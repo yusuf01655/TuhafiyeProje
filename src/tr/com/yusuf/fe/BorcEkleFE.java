080046809008efc8021c0c490d7474b6d135cdef
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-17 07:14:02 +0300

    BorcDuzenleFE, BorcEkleFE, BorcGosterFE, siniflari olusturuldu

---
+++ src/tr/com/yusuf/fe/BorcEkleFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class BorcEkleFE implements FeInterfaces {
+
+	public BorcEkleFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
