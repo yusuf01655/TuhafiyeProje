dd59b089f8a7a0452c3c750c51a61654d29a09ff
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-17 07:21:00 +0300

    MusteriGosterFE MusteriSilFE PersonelDuzenleFE siniflari olusturuldu.

---
+++ src/tr/com/yusuf/fe/MusteriSilFE.java
@@ -0,0 +1,17 @@
+package tr.com.yusuf.fe;
+
+import tr.com.yusuf.interfaces.FeInterfaces;
+
+public class MusteriSilFE implements FeInterfaces {
+
+	public MusteriSilFE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initPencere() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
