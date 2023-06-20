e5b4afabfbf75147c26ebbb4e3be3ab652eced35
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-20 08:53:32 +0300

    kategori silme olayi atandi ve pencere baslatma fonksiyonu yazildi

--- src/tr/com/yusuf/fe/KategoriSilFE.java
+++ src/tr/com/yusuf/fe/KategoriSilFE.java
@@ -1,5 +1,11 @@
 package tr.com.yusuf.fe;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import tr.com.yusuf.interfaces.FeInterfaces;
 
 public class KategoriSilFE implements FeInterfaces {
@@ -10,8 +16,22 @@ public class KategoriSilFE implements FeInterfaces {
 
 	@Override
 	public void initPencere() {
-		// TODO Auto-generated method stub
+		try {
+			Stage primaryStage = new Stage();
+			AnchorPane root = (AnchorPane) FXMLLoader
+					.load(getClass().getResource("/tr/com/yusuf/fe/KategoriSilFE.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("/tr/com/yusuf/test/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
+	@FXML
+	void menuItemKategoriSil_OnAction(ActionEvent actionEvent) {
+
+	}
 }
