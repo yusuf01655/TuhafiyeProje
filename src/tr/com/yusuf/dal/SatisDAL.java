9b3b102899c29d7fbf8821ddcda73c1ff1566253
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-06-23 17:32:45 +0300

    SatisDAL fonksiyonlari dolduruldu.

--- src/tr/com/yusuf/dal/SatisDAL.java
+++ src/tr/com/yusuf/dal/SatisDAL.java
@@ -1,11 +1,19 @@
 package tr.com.yusuf.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import tr.com.yusuf.complex.types.SatisContractComplex;
 import tr.com.yusuf.core.ObjectHelper;
 import tr.com.yusuf.interfaces.DALInterfaces;
+import tr.com.yusuf.types.SatisContract;
 
-public class SatisDAL<SatisContract> extends ObjectHelper implements DALInterfaces<SatisContract> {
+public class SatisDAL extends ObjectHelper implements DALInterfaces<SatisContract> {
 
 	public SatisDAL() {
 		// TODO Auto-generated constructor stub
@@ -13,32 +21,143 @@ public class SatisDAL<SatisContract> extends ObjectHelper implements DALInterfac
 
 	@Override
 	public void ekle(SatisContract entity) {
-		// TODO Auto-generated method stub
+		Connection connection = getConnection();
+		String sorgu = "INSERT INTO satis(urun_id, musteri_id, personel_id, adet, tarih) VALUES(?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sorgu);
+			preparedStatement.setInt(1, entity.getUrunId());
+			preparedStatement.setInt(2, entity.getMusteriId());
+			preparedStatement.setInt(3, entity.getPersonelId());
+			preparedStatement.setInt(4, entity.getAdet());
+			preparedStatement.setDate(5, entity.getTarih());
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+			connection.close(); // yoksa ifadeyi deðiþtir.
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public List<SatisContract> hepsiniAl() {
-		// TODO Auto-generated method stub
-		return null;
+		List<SatisContract> veriContract = new ArrayList<>();
+		Connection connection = getConnection();
+		SatisContract satisContract;
+		String sorgu = "SELECT satis.satis_id, hesaplar.ad, hesaplar.soyad ,musteri.ad, musteri.soyad, satis.adet, satis.tarih FROM satis LEFT JOIN musteri on satis.musteri_id = musteri.musteri_id LEFT JOIN urunler on satis.urun_id = urunler.urun_id LEFT JOIN hesaplar on satis.personel_id = hesaplar.personel_id ";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sorgu);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				satisContract = new SatisContract();
+				satisContract.setId(resultSet.getInt("satis_id"));
+				satisContract.setMusteriId(resultSet.getInt("musteri_id"));
+				satisContract.setPersonelId(resultSet.getInt("personel_id"));
+				satisContract.setUrunId(resultSet.getInt("urun_id"));
+				satisContract.setAdet(resultSet.getInt("adet"));
+				satisContract.setTarih(resultSet.getDate("tarih"));
+				veriContract.add(satisContract);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return veriContract;
+	}
+
+	public List<SatisContractComplex> satislarinHepsiniAl() {
+		List<SatisContractComplex> veriContract = new ArrayList<>();
+		Connection connection = getConnection();
+		SatisContractComplex satisContractComplex;
+		String sorgu = "SELECT satis.satis_id, hesaplar.ad, hesaplar.soyad ,musteri.ad, musteri.soyad, satis.adet, satis.tarih FROM satis LEFT JOIN musteri on satis.musteri_id = musteri.musteri_id LEFT JOIN urunler on satis.urun_id = urunler.urun_id LEFT JOIN hesaplar on satis.personel_id = hesaplar.personel_id ";
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sorgu);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				satisContractComplex = new SatisContractComplex();
+				satisContractComplex.setSatisId(resultSet.getInt("satis.satis_id"));
+				satisContractComplex.setHesapAdi(resultSet.getString("hesaplar.ad"));
+				satisContractComplex.setHesapSoyadi(resultSet.getString("hesaplar.soyad"));
+				satisContractComplex.setMusteriAd(resultSet.getString("musteri.ad"));
+				satisContractComplex.setMusteriSoyad(resultSet.getString("musteri.soyad"));
+				satisContractComplex.setSatisAdet(resultSet.getInt("satis.adet"));
+				satisContractComplex.setSatisTarih(resultSet.getDate("satis.tarih"));
+				veriContract.add(satisContractComplex);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return veriContract;
 	}
 
 	@Override
 	public SatisContract sil(SatisContract entity) {
-		// TODO Auto-generated method stub
-		return null;
+		SatisContract satisContract = new SatisContract();
+		Connection connection = getConnection();
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("DELETE FROM satislar WHERE satis_id=" + entity.getId());
+			statement.close();
+			connection.close();
+
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		return satisContract;
 	}
 
 	@Override
 	public void guncelle(SatisContract entity) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * private int musteriId; private int personelId; private int urunId; private
+		 * int adet; private Date tarih;
+		 */
+		Connection connection = getConnection();
+		String sorgu = "UPDATE satis SET musteri_id=?, personel_id = ?, urun_id = ?, adet = ?, tarih = ? WHERE satis_id = ?";
+		try {
+			/*
+			 * private int musteriId; private int personelId; private int urunId; private
+			 * int adet; private Date tarih;
+			 */
+			PreparedStatement preparedStatement = connection.prepareStatement(sorgu);
+			preparedStatement.setInt(1, entity.getMusteriId());
+			preparedStatement.setInt(2, entity.getMusteriId());
+			preparedStatement.setInt(3, entity.getUrunId());
+			preparedStatement.setInt(4, entity.getAdet());
+			preparedStatement.setDate(5, entity.getTarih());
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public List<SatisContract> idIleAl(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		List<SatisContract> veriContract = new ArrayList<>();
+		Connection connection = getConnection();
+		SatisContract satisContract;
+		String sorgu = "SELECT satis.satis_id, hesaplar.ad, hesaplar.soyad ,musteri.ad, musteri.soyad, satis.adet, satis.tarih FROM satis LEFT JOIN musteri on satis.musteri_id = musteri.musteri_id LEFT JOIN urunler on satis.urun_id = urunler.urun_id LEFT JOIN hesaplar on satis.personel_id = hesaplar.personel_id WHERE satis_id = "
+				+ id;
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sorgu);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				satisContract = new SatisContract();
+				satisContract.setId(resultSet.getInt("satis_id"));
+				satisContract.setMusteriId(resultSet.getInt("musteri_id"));
+				satisContract.setPersonelId(resultSet.getInt("personel_id"));
+				satisContract.setUrunId(resultSet.getInt("urun_id"));
+				satisContract.setAdet(resultSet.getInt("adet"));
+				satisContract.setTarih(resultSet.getDate("tarih"));
+				veriContract.add(satisContract);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return veriContract;
 	}
 
 }
