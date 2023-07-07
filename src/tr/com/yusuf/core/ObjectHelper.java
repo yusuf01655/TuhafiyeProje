858485b0b3c8202b8b79934f13af0e86f24ef7b3
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-07-07 15:37:23 +0300

    md5 hashleme islemi eklendi.

--- src/tr/com/yusuf/core/ObjectHelper.java
+++ src/tr/com/yusuf/core/ObjectHelper.java
@@ -36,9 +36,18 @@ public class ObjectHelper extends CoreFields implements CoreInterfaces, IMd5Encr
 	@Override
 	public String MD5Encrypt(String girdi) {
 		try {
+
+			// Static getInstance method is called with hashing MD5
 			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			// digest() method is called to calculate message digest
+			// of an input digest() return array of byte
 			byte[] messageDigest = md.digest(girdi.getBytes());
+
+			// Convert byte array into signum representation
 			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
 			String hashtext = no.toString(16);
 			while (hashtext.length() < 32) {
 				hashtext = "0" + hashtext;
