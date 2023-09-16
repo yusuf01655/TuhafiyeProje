9ff621973f94fc5a0e39898164bc8684866a2062
Yusuf Aksoy <yusuf01655@outlook.com.tr>
2023-09-16 11:39:14 +0300

    typelar module dosyasinda tanimlandi

--- src/module-info.java
+++ src/module-info.java
@@ -3,8 +3,10 @@ module TuhafiyeProje {
 	requires javafx.fxml;
 	requires java.sql;
 	requires javafx.graphics;
+	requires javafx.base;
 
 	exports tr.com.yusuf.test;
 
 	opens tr.com.yusuf.fe to javafx.graphics, javafx.fxml;
+	opens tr.com.yusuf.types to javafx.base;
 }
